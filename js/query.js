d1d03ca07292a7bb1b152cd1982c71592bed91b0
SagittariuX <aaronan1996@gmail.com>
2020-03-26 22:15:06 -0400

    typo

--- js/query.js
+++ js/query.js
@@ -3,16 +3,16 @@
 
 $(document).ready(function (){
     //Loads initial information
-    $('#aboutMeText0').load("text/aboutme0.txt");
-    $('#aboutMeText1').load("text/aboutme0.txt");
-    $('#aboutMeText2').load("text/aboutme0.txt");
+    $('#aboutMeText0').load("text/aboutme1.txt");
+    $('#aboutMeText1').load("text/aboutme1.txt");
+    $('#aboutMeText2').load("text/aboutme1.txt");
     $('#compSciText').load("text/aboutme0.txt");
     $('#businessText').load("text/aboutme0.txt");
     $('#educationText').load("text/aboutme0.txt");
 
     //sets up intial slides
     $.each(compSciSlides, function(index, value){
-        value.hide()
+        value.hide();
     });
 
     compSciSlideShow(compSciSlide);
