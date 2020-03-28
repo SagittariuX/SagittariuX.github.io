fc00a74f4b658e6768af23b142dc756d8172a370
SagittariuX <aaronan1996@gmail.com>
2020-03-28 14:57:34 -0400

    transfered initial data loading to query.js

--- js/query.js
+++ js/query.js
@@ -1,10 +1,15 @@
 
 $(document).ready(function (){
     //Loads initial information
-    $('#aboutMeText0').load("text/aboutme1.txt");
-    $('#aboutMeText1').load("text/aboutme1.txt");
-    $('#aboutMeText2').load("text/aboutme1.txt");
-    $('#compSciText').load("text/aboutme0.txt");
-    $('#businessText').load("text/aboutme0.txt");
-    $('#educationText').load("text/aboutme0.txt");
+    $('#aboutMeText0').load("text/intro/aboutme1.txt");
+    $('#aboutMeText1').load("text/intro/aboutme1.txt");
+    $('#aboutMeText2').load("text/intro/aboutme1.txt");
+    $('#compSciText').load("text/compSci/aboutme0.txt");
+    $('#businessText').load("text/business/aboutme0.txt");
+    $('#educationText').load("text/education/aboutme0.txt");
+
+    //Loads inital pics
+    $('#compSciPic').prop('src', 'pics/compSci/placeholder0.png');
+    $('#businessPic').prop('src', 'pics/business/placeholder0.png');
+    $('$educationPic').prop('src', 'pics/education/placeholder0.png');
 });
