853cc2f557147e140d4f8658a1164ab068559fd0
SagittariuX <aaronan1996@gmail.com>
2020-04-04 21:48:13 -0400

    forgot to initialize content

--- js/query.js
+++ js/query.js
@@ -10,12 +10,14 @@ $(document).ready(function (){
     $('#businessText').load("text/business/aboutme1.txt");
     $('#businessTextMobile').load("text/business/aboutme1.txt");
     $('#educationText').load("text/education/aboutme1.txt");
+    $('#educationTextMobile').load("text/education/aboutme1.txt");
 
     //Loads inital pics
     $('#compSciPic').prop('src', 'pics/compSci/placeholder0.png');
     $('#businessPic').prop('src', 'pics/business/placeholder0.png');
     $('#businessPicMobile').prop('src', 'pics/business/placeholder0.png');
     $('#educationPic').prop('src', 'pics/education/placeholder0.png');
+    $('#educationPicMobile').prop('src', 'pics/education/placeholder0.png');
 });
 
 $(window).on('beforeunload', function() {
