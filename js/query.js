1b8e19ea89daba9895f7f6f2b2a071e4982e971b
SagittariuX <aaronan1996@gmail.com>
2020-04-12 05:05:30 -0400

    added recuring animation to dropdown menu

--- js/query.js
+++ js/query.js
@@ -20,6 +20,8 @@ $(document).ready(function (){
     $('#educationPicMobile').prop('src', 'pics/education/placeholder0.png');
 });
 
+// Makes sure to rewind viewing history to top so animations could playout
+// Only chrome needs to have this explicit command
 $(window).on('beforeunload', function() {
     $(window).scrollTop(0);
 });
@@ -33,10 +35,20 @@ $(window).on('scroll', function(){
     else {
         $('#navbar').removeClass('sticky');
     }
-    if ($(window).scrollTop() > navbarOffset){
+    if ($(window).scrollTop() > navbarMobileOffset){
         $('#navbarMobile').addClass('sticky');
     }
     else {
         $('#navbarMobile').removeClass('sticky');
     }
 });
+
+// enables animation everytime hover happens
+$("#navbarMobile").hover(
+    function(){
+        $("#navbarMobileDropDown").addClass("wow slideInDown");
+    }, 
+    function(){
+        $("#navbarMobileDropDown").removeClass("wow slideInDown")
+    }
+);
