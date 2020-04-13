1cd7d8c85f5d28e95568eae6ae705f592f4eb2e2
SagittariuX <aaronan1996@gmail.com>
2020-04-13 04:32:36 -0400

    adding text to checkout format

--- js/query.js
+++ js/query.js
@@ -2,23 +2,32 @@
 $(document).ready(function (){
     $(this).scrollTop(0);
     
-    //Loads initial information
+    //Loads aboutme info
     $('.aboutMeText').each(function(index, element){
         $(element).load("text/intro/aboutme1.txt");
     });
-    $('#compSciText').load("text/compSci/aboutme1.txt");
-    $('#businessText').load("text/business/aboutme1.txt");
-    $('#businessTextMobile').load("text/business/aboutme1.txt");
-    $('#educationText').load("text/education/aboutme1.txt");
-    $('#educationTextMobile').load("text/education/aboutme1.txt");
 
-    //Loads inital pics
-    $('#compSciPic').prop('src', 'pics/compSci/placeholder0.png');
+    //loads experience info
     $('.experienceMobilePicture').each(function(index, element){
         $(element).prop('src', 'pics/experience/placeholder'+index+'.png');
     });
+    $('.experienceMobileText').each(function(index, element){
+        $(element).load('text/experience/aboutme'+index+'.txt');
+    });
+
+    //loads compsci info
+    $('#compSciText').load("text/compSci/aboutme1.txt");
+    $('#compSciPic').prop('src', 'pics/compSci/placeholder0.png');
+
+    //loads business info
+    $('#businessText').load("text/business/aboutme1.txt");
+    $('#businessTextMobile').load("text/business/aboutme1.txt");
     $('#businessPic').prop('src', 'pics/business/placeholder0.png');
     $('#businessPicMobile').prop('src', 'pics/business/placeholder0.png');
+
+    //Loads education info
+    $('#educationText').load("text/education/aboutme1.txt");
+    $('#educationTextMobile').load("text/education/aboutme1.txt");
     $('#educationPic').prop('src', 'pics/education/placeholder0.png');
     $('#educationPicMobile').prop('src', 'pics/education/placeholder0.png');
 });
