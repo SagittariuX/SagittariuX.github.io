182f734c57cf321783f75b3697f96a208034ca67
SagittariuX <aaronan1996@gmail.com>
2020-04-26 10:08:01 -0400

    finished with new slideshow

--- js/query.js
+++ js/query.js
@@ -71,10 +71,21 @@ $(document).ready(function (){
     });
 
     //loads compsci info
-    for(var $i = 0; $i < $gCompSci ; $i++){
-        $('.compSciSlidesFrame').append(`
-            <input type="radio" name="compSciPics" id="pic`+$i+`">
-            <label for="pic`+$i+`" id="compSciPic`+$i+`></label> 
+    for(var $i = 0; $i < $gCompSci ; $i++){//generates radio buttons
+        if($i == 0){
+            $('.compSciSlidesFrame form').append(`
+                <input type="radio" name="compSciPics" id="cspic`+$i+`" checked>
+            `);
+        }
+        else{
+            $('.compSciSlidesFrame form').append(`
+                <input type="radio" name="compSciPics" id="cspic`+$i+`">
+            `);
+        }
+    }
+    for(var $i = 0 ; $i < $gCompSci ; $i++){//generates the visuals for buttons
+        $('.compSciSlidesFrame form').append(`
+            <label for="cspic`+$i+`" id="compSciPic`+$i+`"></label> 
         `);
     }
     $('#compSciText').load("text/compSci/aboutme1.txt");
