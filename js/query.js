3b549f42c6d4d2a13c792d164d8331c8b0ca30b4
SagittariuX <aaronan1996@gmail.com>
2020-04-26 13:46:19 -0400

    added new img to slide show, will test other images later

--- js/query.js
+++ js/query.js
@@ -85,9 +85,13 @@ $(document).ready(function (){
     }
     for(var $i = 0 ; $i < $gCompSci ; $i++){//generates the visuals for buttons
         $('.compSciSlidesFrame form').append(`
-            <label for="cspic`+$i+`" id="compSciPic`+$i+`"></label> 
+            <label for="cspic`+$i+`" id="compSciPic`+$i+`">
+                <img src="pics/compSci/pic`+$i+`.png" />
+            </label> 
         `);
     }
+
+
     $('#compSciText').load("text/compSci/aboutme1.txt");
     
 
