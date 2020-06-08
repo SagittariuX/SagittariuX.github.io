fdde42b5ec1fe89a75ad9f995aff0d372bd99924
SagittariuX <aaronan1996@gmail.com>
2020-06-07 23:56:39 -0400

    figuring how to make images look good

--- js/swipe.js
+++ js/swipe.js
@@ -5,14 +5,8 @@ galleryHandler.on('swipeleft swiperight', function(event){
 });
 
 var playlistHandler = new Hammer($('#spotifyPlaylist')[0]);
-
-playlistHandler.on('swipeleft', function(event){
-    formatTracks($gFocusTrack+1, event.type);
+playlistHandler.on('swipeleft swiperight', function(event){
+    (event.type === 'swipeleft') ? formatTracks($gFocusTrack+1, event.type) : formatTracks($gFocusTrack-1, event.type);    
     $gSwipeTimeout = Date.now() + 100;
-    console.log(event.type);
-});
-playlistHandler.on('swiperight', function(event){
-    formatTracks($gFocusTrack-1, event.type);
-    $gSwipeTimeout = Date.now() + 100;
-    console.log(event.type);
+    console.log('spotifyPlaylist: '+event.type);
 });
